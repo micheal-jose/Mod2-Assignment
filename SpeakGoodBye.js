cc8f2a6e5e3e64237007d5c784abd4688f68b3e6
micheal-jose <65111333+micheal-jose@users.noreply.github.com>
2020-06-01 20:31:44 +0530

    Create SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (Window){
+  var byeSpeaker = {};
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(Window);
